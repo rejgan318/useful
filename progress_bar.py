12cd068f1f9986d0c05fa7ac8cdc08a4be38789e
rejgan318 <rejgan318@gmail.com>
2022-05-21 17:53:15 +0300

    Оформлено

--- progress_bar.py
+++ progress_bar.py
@@ -3,12 +3,23 @@ progress bar
 """
 
 
-def progress_bar(progress, total, symbol='█', width=100):
+def progress_bar(progress: float | int, total: float | int, symbol: str = '█', width: int = 100) -> None:
+    """
+    горизонтальный прогресс-бар
+    :param progress: текущеее значение
+    :param total: максимальное значение
+    :param symbol: символ вывода прогресса
+    :param width: ширина в символах
+    :return:
+    """
     print(f'{symbol * int(width * progress / total):.<{width}} {round(progress / total * 100):3}%', end='\r')
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     import math
+
+    print('😀😁😂🤣😃😄😅😆ꊠ黠だ')
     n = 100
     for i in range(n):
         result = math.factorial(i * 1000)
-        progress_bar(i+1, n, width=20, symbol='*')
+        # Win + . - select unicode char
+        progress_bar(i+1, n, width=20)
